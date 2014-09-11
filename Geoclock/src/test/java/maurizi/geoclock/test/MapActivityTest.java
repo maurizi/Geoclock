28d37429dd772ec9dc3383fbc2ebf76ad7763164
Michael Maurizi <hollaburoo@gmail.com>
2014-09-10 22:35:00 -0400

    Add MapActivity.onResume test

--- Geoclock/src/test/java/maurizi/geoclock/test/MapActivityTest.java
+++ Geoclock/src/test/java/maurizi/geoclock/test/MapActivityTest.java
@@ -13,11 +13,13 @@ import org.robolectric.util.ActivityController;
 import maurizi.geoclock.MapActivity;
 import maurizi.geoclock.NavigationDrawerFragment;
 import maurizi.geoclock.R;
+import maurizi.geoclock.test.shadows.ShadowSupportMenuInflater;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk = 18)
+@Config(emulateSdk = 18, shadows = {ShadowSupportMenuInflater.class})
 public class MapActivityTest {
 
 	private ActivityController<MapActivity> controller;
@@ -32,5 +34,10 @@ public class MapActivityTest {
 		MapActivity activity = controller.create().get();
 		assertThat(activity.getSupportFragmentManager().findFragmentById(R.id.navigation_drawer)).isInstanceOf(NavigationDrawerFragment.class);
 		assertThat(activity.getSupportFragmentManager().findFragmentById(R.id.map)).isInstanceOf(SupportMapFragment.class);
+	}
+
+	@Test
+	public void test_onResume() {
+		assertNotNull(controller.create().start().resume().get());
 	}
 }
